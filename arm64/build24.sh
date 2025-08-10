9b4497379c5cb5620041c3b72158ac6b4f1b28b7
Kwonelee <995546054@qq.com>
2025-08-10 12:05:43 +0800

    Update build24.sh

--- arm64/build24.sh
+++ arm64/build24.sh
@@ -35,10 +35,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始构建固件..."
 # 初始化变量
 PACKAGES=""
 
-<details>
-<summary>📦 官方集成列表（点击展开）</summary>
-
-```bash
+{
 PACKAGES="$PACKAGES adb"
 PACKAGES="$PACKAGES adb-enablemodem"
 #PACKAGES="$PACKAGES appfilter"
@@ -1021,8 +1018,7 @@ PACKAGES="$PACKAGES xz-utils"
 PACKAGES="$PACKAGES zlib"
 PACKAGES="$PACKAGES zram-swap"
 PACKAGES="$PACKAGES -libustream-mbedtls"
-```
-</details>
+}
 
 # O大打包脚本补充插件，其他依赖官方列表有集成
 PACKAGES="$PACKAGES perlbase-time"
