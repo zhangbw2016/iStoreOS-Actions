4df6cd5891bd9216aaaa1842f8936ee7d276c13e
Kwonelee <995546054@qq.com>
2025-09-15 20:31:20 +0800

    增加luci-app-adguardhome

--- arm64/build24.sh
+++ arm64/build24.sh
@@ -1054,6 +1054,7 @@ PACKAGES="$PACKAGES luci-app-amlogic luci-i18n-amlogic-zh-cn"
 PACKAGES="$PACKAGES lucky luci-app-lucky luci-i18n-lucky-zh-cn"
 PACKAGES="$PACKAGES openlist2 luci-app-openlist2 luci-i18n-openlist2-zh-cn"
 PACKAGES="$PACKAGES luci-app-ramfree luci-i18n-ramfree-zh-cn"
+PACKAGES="$PACKAGES luci-app-adguardhome luci-i18n-adguardhome-zh-cn"
 
 # opc-rely依赖，其他会自动集成
 PACKAGES="$PACKAGES ruby ruby-pstore ruby-psych ruby-yaml"
